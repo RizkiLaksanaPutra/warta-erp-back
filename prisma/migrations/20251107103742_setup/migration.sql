30561d297faddfa85760ea802696d42b71c385c3
Rizki Laksana Putra <120088587+RizkiLaksanaPutra@users.noreply.github.com>
2025-11-07 18:56:26 +0700

    update prisma

---
+++ prisma/migrations/20251107103742_setup/migration.sql
@@ -0,0 +1,64 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(36) NOT NULL,
+    `email` VARCHAR(200) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `token` VARCHAR(100) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `branches` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` VARCHAR(36) NOT NULL,
+    `code` VARCHAR(20) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `phone` VARCHAR(20) NULL,
+    `street` VARCHAR(255) NULL,
+    `city` VARCHAR(100) NULL,
+    `province` VARCHAR(100) NULL,
+    `postal_code` VARCHAR(10) NULL,
+    `start_hours` VARCHAR(8) NULL,
+    `end_hours` VARCHAR(8) NULL,
+    `is_active` BOOLEAN NOT NULL DEFAULT true,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `branches_code_key`(`code`),
+    INDEX `branches_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `employees` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` VARCHAR(36) NOT NULL,
+    `branchId` INTEGER NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `phone` VARCHAR(20) NULL,
+    `position` VARCHAR(60) NOT NULL,
+    `salary` INTEGER NOT NULL,
+    `join` DATETIME(3) NOT NULL,
+    `status` ENUM('ACTIVE', 'RESIGNED') NOT NULL DEFAULT 'ACTIVE',
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    INDEX `employees_userId_idx`(`userId`),
+    INDEX `employees_branchId_idx`(`branchId`),
+    INDEX `employees_userId_branchId_status_idx`(`userId`, `branchId`, `status`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `branches` ADD CONSTRAINT `branches_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `employees` ADD CONSTRAINT `employees_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `employees` ADD CONSTRAINT `employees_branchId_fkey` FOREIGN KEY (`branchId`) REFERENCES `branches`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
